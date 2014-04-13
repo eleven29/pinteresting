4e6210ae4bba9dfa8cd0011a49ea286e73073b7c
Yannick Posse <yannick.posse@gmail.com>
2014-04-13 22:06:51 +0200

    fix S3 on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,16 @@ Pinteresting::Application.configure do
 
   # sets paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+     }
+    :Attachment.default_options[:url] = ':s3_domain_url'
+    :Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'}
+  }  
+
+
 
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,3 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 require 'paperclip/media_type_spoof_detector'
 module Paperclip
