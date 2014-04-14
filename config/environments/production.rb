3ecc5ae25c4ffe7ce514dc99ee0fbca553e3a190
Yannick Posse <yannick.posse@gmail.com>
2014-04-14 10:54:58 +0200

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,9 +89,9 @@ Pinteresting::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-     }
-    :Attachment.default_options[:url] = ':s3_domain_url'
-    :Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'}
+     },
+    :Attachment.default_options[:url] = ':s3_domain_url',
+    :Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename',
   }  
 
 
